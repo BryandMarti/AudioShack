d445b69c78b3823e5903fa2e9df2a6b499cb1a4e
BryandMarti <164084388+BryandMarti@users.noreply.github.com>
2024-07-23 07:56:44 -0400

    always some little mispelling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react';
 import './scripts_css/across.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
-import logo from './staticImgs/audiologo.png';
+import logo from './staticImgs/Audiologo.png';
 import Home from './jsxClient/home';
 import ContactUs from './jsxClient/ContactUs';
 import Products from './jsxClient/Products';
